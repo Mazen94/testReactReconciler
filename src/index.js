0b7069340995127f03aba902dff19cd626f1b1d6
ELAYEB Mazen <melayeb@anypli.com>
2022-10-30 09:09:30 +0100

    First Test of reconciler

--- src/index.js
+++ src/index.js
@@ -1,15 +1,11 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import MyCustomRenderer from './myCustomRenderer';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+console.log({App: App()})
+MyCustomRenderer.render(<App />, document.getElementById('root'));
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
---
+++ src/myCustomRenderer.js
@@ -0,0 +1,55 @@
+import ReactReconciler from 'react-reconciler';
+
+
+
+const hostConfig = {  
+  getRootHostContext: () => {},
+  prepareForCommit: () => {},
+  resetAfterCommit: () => {},
+  getChildHostContext: () => {},
+  shouldSetTextContent: () => {},
+  /**
+   This is where react-reconciler wants to create an instance of UI element in terms of the target. Since our target here is the DOM, we will create document.createElement and type is the argument that contains the type string like div or img or h1 etc. The initial values of domElement attributes can be set in this function from the newProps argument
+   */
+  createInstance: (type, newProps, rootContainerInstance, _currentHostContext, workInProgress) => {
+    const domElement = document.createElement(type);
+    Object.keys(newProps).forEach(propName => {
+      const propValue = newProps[propName];
+      if (propName === 'className') {
+        domElement.setAttribute('class', propValue);
+      } else {
+        const propValue = newProps[propName];
+        domElement.setAttribute(propName, propValue);
+      }
+    });
+    return domElement;
+  },
+  createTextInstance: text => {
+    return document.createTextNode(text);
+  },
+  appendInitialChild: (parent, child) => {
+    parent.appendChild(child);
+  },
+  appendChild(parent, child) {
+    parent.appendChild(child);
+  },
+  finalizeInitialChildren: (domElement, type, props) => {},
+  supportsMutation: true,
+  appendChildToContainer: (parent, child) => {
+    parent.appendChild(child);
+  },
+  
+};
+const ReactReconcilerInst = ReactReconciler(hostConfig);
+const MyCustomRenderer = {
+  render: (reactElement, domElement, callback) => {
+    // Create a root Container if it doesnt exist
+    if (!domElement._rootContainer) {
+      domElement._rootContainer = ReactReconcilerInst.createContainer(domElement, false);
+    }
+
+    // update the root Container
+    return ReactReconcilerInst.updateContainer(reactElement, domElement._rootContainer, null, callback);
+  }
+};
+export default MyCustomRenderer
